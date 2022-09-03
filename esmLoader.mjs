9c7a430e5347b7e36b5a35b41885fcb40dfa6743
Guy Bedford <guybedford@gmail.com>
2022-09-03 13:26:03 -0700

    syncify (#2)

--- esmLoader.mjs
+++ esmLoader.mjs
@@ -1,36 +1,20 @@
-import { MessageChannel, Worker } from 'node:worker_threads';
+import { Worker } from 'node:worker_threads';
 
-const lock = new Int32Array(new SharedArrayBuffer(4));
-const worker = new Worker('./worker.mjs', {
-	workerData: {
-		lock,
-	}
-});
+const buf = new SharedArrayBuffer(2048);
+const lock = new Int32Array(buf, 0, 4);
 
-Atomics.wait(lock, 0, 0);
+// request / response memory space
+const data = new Uint8Array(buf, 4, 2044);
+
+const worker = new Worker('./worker.mjs', {	workerData: { buf } });
+
+await new Promise(resolve => worker.on('message', resolve));
 
 export function importMetaResolve(specifier) {
-	let output;
-
-	const lock = new Int32Array(new SharedArrayBuffer(4));
-	const {
-		port1: responder,
-		port2: requestor,
-	} = new MessageChannel();
-	responder.once('message', (msg) => {
-		console.log(process.hrtime(), `[ESMLoader]: incoming message '${msg }'`);
-		output = msg;
-	});
-	worker.postMessage({
-		lock,
-		requestor,
-		type: 'resolve',
-		value: specifier,
-	}, [requestor]);
-
-	Atomics.wait(lock, 0, 0);
-
-	console.log(process.hrtime(), `[ESMLoader]: '${specifier}' awakened with '${output}'`);
-
-	return output;
+	data.fill(0);
+	new TextEncoder().encodeInto(specifier, data);
+	Atomics.store(lock, 0, 1);
+	Atomics.notify(lock, 0);
+	Atomics.wait(lock, 0, 1);
+	return new TextDecoder().decode(data);
 }
