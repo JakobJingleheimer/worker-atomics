474c5ce0f7808f4d1986c07c26e741107762d65c
Michaël Zasso <targos@protonmail.com>
2023-01-01 13:03:36 +0100

    unlock worker

--- esmLoader.mjs
+++ esmLoader.mjs
@@ -1,28 +1,52 @@
-import { deserialize, serialize } from 'node:v8';
-import { Worker } from 'node:worker_threads';
+import { Worker, MessageChannel, receiveMessageOnPort } from 'node:worker_threads';
 
-const commsChannel = new SharedArrayBuffer(2048);
-const lock = new Int32Array(commsChannel, 0, 4); // required by Atomics
+const lock = new Int32Array(new SharedArrayBuffer(4)); // required by Atomics
 // lock = 0 → main sleeps
-// lock = 1 → worker sleeps
-const requestResponseData = new Uint8Array(commsChannel, 4, 2044); // for TextEncoder/TextDencoder
+
+const synchronousCommChannel = new MessageChannel();
 
 const worker = new Worker('./worker.mjs', {
-	workerData: { commsChannel },
+	workerData: { lock, commPort: synchronousCommChannel.port2 },
+	transferList: [synchronousCommChannel.port2]
 });
-worker.unref(); // ! Allow the process to eventually exit when worker is in its final sleep.
 
-Atomics.wait(lock, 0, 0); // ! Block this module until the worker is ready.
+let jobId = 0;
+const jobs = new Map();
+worker.on('message', ({id, message}) => {
+	jobs.get(id).resolve(message);
+	jobs.delete(id);
+});
+
+lockThread();
 
 export function importMetaResolve(specifier) {
-	requestResponseData.fill(0);
-	const request = serialize({
-		data: specifier,
-		type: 'resolve',
+	return doSyncWork({data: specifier, type: 'resolve'});
+}
+
+
+export function asyncResolve(specifier) {
+	return doAsyncWork({data: specifier, type: 'resolve'});
+}
+
+export function terminate() {
+	worker.terminate();
+}
+
+function doSyncWork(message) {
+	synchronousCommChannel.port1.postMessage(message);
+	lockThread();
+	return receiveMessageOnPort(synchronousCommChannel.port1).message;
+}
+
+function doAsyncWork(message) {
+	return new Promise((resolve) => {
+		const id = jobId++;
+		jobs.set(id, {resolve});
+		worker.postMessage({id, ...message});
 	});
-	requestResponseData.set(request);
-	Atomics.store(lock, 0, 0); // send request to worker
-	Atomics.notify(lock, 0); // notify worker of new request
-	Atomics.wait(lock, 0, 0); // sleep until worker responds
-	return deserialize(requestResponseData);
+}
+
+function lockThread() {
+	Atomics.store(lock, 0, 0);
+	Atomics.wait(lock, 0, 0);
 }
